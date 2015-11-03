3718970a690af3a57d00ff5422cf9d126a412684
apatriz <patrizio@msu.edu>
2015-11-03 15:47:54 -0500

    Update
    -consolidated workflow functions into download_data

--- download_data.py
+++ download_data.py
@@ -6,13 +6,13 @@ import shutil
 from contextlib import closing
 import urlparse
 import time
+import csv
+import itertools
 
 ##sample url from USGS TNM Access API
 ##'''http://viewer.nationalmap.gov/tnmaccess/api/products?
 ##datasets=National+Elevation+Dataset+%28NED%29+1%2F3+arc-second&bbox=-88.117798672%2C38.028972825%2C-88.117608031%2C38.029123082&q=&
 ##prodFormats=IMG&prodExtents=1+x+1+degree&dateType=dateCreated&start=&end=&polyCode=&polyType=&offset=&max=&outputFormat=JSON'''
-site_url = 'http://viewer.nationalmap.gov/tnmaccess/api/products'
-
 
 
 def get_site_extents(site_feature):
@@ -38,24 +38,20 @@ def get_products(site_extent,dataset,product_format,product_extent):
 	>>> 'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/NED/13/IMG/n40w098.zip'
   
 	'''
-##    site_extent = '-88.117798672,38.028972825,-88.117608031,38.029123082'
-##    dataset = 'National Elevation Dataset (NED) 1/3 arc-second'
-##    product_format = 'IMG'
-##    product_extent = '1 x 1 degree'
-	products = []
+	site_url = 'http://viewer.nationalmap.gov/tnmaccess/api/products'
 	#set parameters for http request to TNM Access API 
 	payload = {'datasets':dataset,'bbox':site_extent,
 			   'q':'','prodFormats':product_format,'prodExtents':product_extent,
 			   'dateType':'dateCreated','start':'','end':'',
 			   'polyCode':'','polyType':'', 'offset':'','max':'','outputFormat':'JSON'}
-	
+	products = []
 	r = requests.get(site_url,params = payload)
 	if r.status_code == requests.codes.ok:
 		response = r.json()
 		#check if there are results 
 		if 'total' in response:
 			items = response['items']
-			#append each item object to list 
+			#append each item object to products list 
 			for i in items:
 				products.append(i)
 			return products 
@@ -65,7 +61,67 @@ def get_products(site_extent,dataset,product_format,product_extent):
 	else:
 		r.raise_from_status()
 	time.sleep(2)
+	
+	
+def generate_product_table(site_extents,dataset,product_format,product_extent):
+	'''(dict,str,str,str) -> list of lists
+	
+	Takes input dict containing site names and site extents. Dataset,
+	product format and product extent parameter strings are set according to
+	the USGS TNM Access API: http://viewer.nationalmap.gov/tnmaccess/api/productsForm
+	For each site extent entry, a list of dataset product objects is generated (get_link) for datasets that
+	cover the site extent. If no products were found, the site name is logged.
+	Each download url is checked for duplicates in the final url list 
+	("urls[3]"). Next, the product title,fileformat,boundingbox,url,thumbnail, and metadata value are appended to 
+	their respective sublists, with each sublist representing a "column" in the data structure. 
+	Each "row" is a dataset product, and is represented by index number.
+	i.e. index 0 ("row 1")of sublist 0 ("column 1") refers to the same dataset product as index 0 ("row 1") of sublist 1 ("column 2").
+	This process repeats for each site extent. 
+	Returns the final list of lists (i.e. list of "columns"). 
+	The output is intended to be converted to a csv and used with the TNM Download Manager for bulk downloading of the datasets.
+	
+	'''
+	
+	urls =[['Title'],['FileFormat'],['BoundingBox'],['URL'],['Thumbnail'],['Metadata']]
+	missing_datasets = []
+	for site_name,site_extent in site_extents.items():
+		product_list = get_products(site_extent,dataset,product_format,product_extent)
+		if not product_list:
+			missing_datasets.append(site_name)
+		else:
+			for i in product_list:
+				if i['downloadURL'] not in urls[3]:
+					urls[0].append(i['title'])
+					urls[1].append(i['format'])
+					urls[2].append('{0},{1},{2},{3}'.format(i['boundingBox']['minX'],i['boundingBox']['minY'],i['boundingBox']['maxX'],i['boundingBox']['maxY']))
+					urls[3].append(i['downloadURL'])
+					urls[4].append(i['previewGraphicURL'])
+					urls[5].append(i['metaUrl'])
+	#TODO: output to logfile instead of print to console
+	print "No datasets found for: ",missing_datasets
+	return urls
 
+
+def convert_to_csv(download_urls):
+	''' (list of lists) -> full path string to csv file 
+	Takes an input list of lists. 
+	Precondition: Each sublist should represent a column of values. The indexes of each sublist should
+	correspond to each dataset. i.e. index 0 for each sublist should represent values from the same dataset. 
+	Outputs to csv file and returns the output path.
+	'''
+	with open('all_download_urls.csv','wb') as file:
+		wr = csv.writer(file)
+		for row in itertools.izip_longest(*download_urls,fillvalue=''):
+			wr.writerow(row)
+	return os.path.abspath('all_download_urls.csv')
+	
+
+
+def open_download_manager():
+	try:
+		os.startfile('TNMDownloadManager__V1.2.jar')
+	except Exception, e:
+		print str(e)
 
 
 
--- workflow_script.py
+++ workflow_script.py
@@ -1,12 +1,8 @@
 # workflow script for processing NED
 
-import arcpy
-import os
 import list_datasets
 import download_data
 import re
-import csv
-import itertools
 
 #feature class containing sites
 site_feature = 'C:/Fakhoury_DataMining/Fakhoury.gdb/Fakhoury_Polygons'
@@ -19,65 +15,9 @@ product_extent = '1 x 1 degree'
 
 
 #get download links
-def get_all_links(site_extents,dataset,product_format,product_extent):
-	'''(dict,str,str,str) -> list of lists
-	
-	Takes input dict containing site names and site extents. Dataset,
-	product format and product extent parameter strings are set according to
-	the USGS TNM Access API: http://viewer.nationalmap.gov/tnmaccess/api/productsForm
-	For each site extent entry, a list of dataset product objects is generated (get_link) for datasets that
-	cover the site extent. If no products were found, the site name is logged.
-	Each download url is checked for duplicates in the final url list 
-	("urls[3]"). Next, the product title,fileformat,boundingbox,url,thumbnail, and metadata value are appended to 
-	their respective lists, with each list representing a "column" in the data structure. 
-	This process repeats for each site extent. 
-	Returns the final list of "columns". 
-	The output is intended to be converted to a csv and used with the TNM Download Manager for bulk downloading of the datasets.
-	
-	'''
-	
-	urls =[['Title'],['FileFormat'],['BoundingBox'],['URL'],['Thumbnail'],['Metadata']]
-	missing_datasets = []
-	for site_name,site_extent in site_extents.items():
-		product_list = download_data.get_products(site_extent,dataset,product_format,product_extent)
-		if not product_list:
-			missing_datasets.append(site_name)
-		else:
-			for i in product_list:
-				if i['downloadURL'] not in urls[3]:
-					urls[0].append(i['title'])
-					urls[1].append(i['format'])
-					urls[2].append('{0},{1},{2},{3}'.format(i['boundingBox']['minX'],i['boundingBox']['minY'],i['boundingBox']['maxX'],i['boundingBox']['maxY']))
-					urls[3].append(i['downloadURL'])
-					urls[4].append(i['previewGraphicURL'])
-					urls[5].append(i['metaUrl'])
-	#TODO: output to logfile instead of print to console
-	print "No datasets found for: ",missing_datasets
-	return urls
-
-#convert to csv
-def convert_to_csv(download_urls):
-	''' (list of lists) -> full path string to csv file 
-	Takes an input list of lists. 
-	Precondition: Each sublist should represent a column of values. The indexes of each sublist should
-	correspond to each dataset. i.e. index 0 for each sublist should represent values from the same dataset. 
-	Outputs to csv file and returns the output path.
-	'''
-	with open('all_download_urls.csv','wb') as file:
-		wr = csv.writer(file)
-		for row in itertools.izip_longest(*download_urls,fillvalue=''):
-			wr.writerow(row)
-	return os.path.abspath('all_download_urls.csv')
-	
 
 # Download the data 
-# Manually open the TNM bulk download manager and import the newly created csv file to queue up all the downloads 
-
-def open_download_manager():
-	try:
-		os.startfile('TNMDownloadManager__V1.2.jar')
-	except Exception, e:
-		print str(e)
+# open the TNM bulk download manager and import the newly created csv file to queue up all the downloads 
 
 # Mosiac all datasets
 
@@ -104,20 +44,20 @@ def main():
 	# print len(site_extents)
 	# get all download links
 	print "Retrieving all available datasets..."
-	all_links = get_all_links(site_extents,dataset,product_format,product_extent)
+	product_table = download_data.generate_product_table(site_extents,dataset,product_format,product_extent)
 	# link_names = []
-	# for i in all_links[0]:
+	# for i in product_table[0]:
 		# n = re.findall(r'n[0-9][0-9]w[0-9]{3}',i)
 		# print n 
 		# # link_names.append(n[0])	
 	# print link_names
-	print "Total number of datasets to download: ",len(all_links[3])-1 
-	# print all_links
+	print "Total number of datasets to download: ",len(product_table[3])-1 
+	# print product_table
 	print "Converting to csv..."
-	csv_file = convert_to_csv(all_links)
+	csv_file = download_data.convert_to_csv(product_table)
 	print "CSV file saved to: {0}".format(csv_file)
 	print "Opening TNM Download Manager..." 
-	open_download_manager()
+	download_data.open_download_manager()
 	# already_downloaded = list_datasets.list_datasets('C:\\Fakhoury_DataMining\\NED_10m\\raw_data',datatype='RasterDataset',type='IMG')
 	# name_list = []
 	# for i in already_downloaded:
